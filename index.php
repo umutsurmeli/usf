b2ea317d7f5f8fe1863ee7d90c0a374e626d06ab
umut <umut@umut-HP-Notebook>
2021-06-24 00:14:33 +0300

    siliyoruz

--- index.php
+++ index.php
@@ -10,11 +10,11 @@ define('DOMAIN',$_SERVER['HTTP_HOST']);
 define('PROTOCOL',$_SERVER['REQUEST_SCHEME'].'://');
 
 define('DEFAULT_CONTROLLER','underconstruction');
-define('US_VERSION','V.1.013');
+define('US_VERSION','V.1.016');
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);//E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);//E_ALL);
 
 if(DOMAIN=='localhost'||DOMAIN==''||$_SERVER['REQUEST_SCHEME']=='http') {
     session_set_cookie_params(86400, '/','.'.DOMAIN, false,false); //httponly = false javascript için önemli
@@ -22,7 +22,6 @@ if(DOMAIN=='localhost'||DOMAIN==''||$_SERVER['REQUEST_SCHEME']=='http') {
 else {
     session_set_cookie_params(86400, '/','.'.DOMAIN, true,true); //httponly = false javascript için önemli
 }
-//include(SYSTEMPATH.'controller/us_controller.php');
 
 session_start();
 include(SYSTEMPATH.'starter.php');
--- system/library/session.php
+++ system/library/session.php
@@ -2,22 +2,63 @@
 namespace umutsurmeli\system\library;
 //use umutsurmeli\system\controller\us_controller;
 class session {
-    private $_flashdata;
-    private $_tempdata;
+    //private $_flashdatalist;
+    //private $_tempdatalist;
+    public static $flashdatatemizlendi = 0;
 
     public function __construct() {
-        foreach($_SESSION as $key=>$value) {
-            if(!property_exists($this, $key)) {
-            $this->{$key} = $value;
+        $this->flashdataclear();
+        $this->tempdataclear();
+
+        
+}
+
+/**
+ * 
+ * @param type $key tek kullanımlık oturum değişkeni adı.
+ * @param type $value tek kullanımlık oturum değişkeni değeri.
+ */
+public function flashdata($key,$value) {
+    $_SESSION[$key] = $value;
+    $_SESSION['_flashdatalist'][$key] = 0;
+}
+/**
+ * 
+ * @param type $key süreli kullanımlı oturum değişkeni adı.
+ * @param type $value süreli kullanımlı oturum değişkeni değeri.
+ * @param type $second oturum değişkeni kullanım ömrü (saniye cinsinden).
+ */
+public function tempdata($key,$value,$second) {
+    $_SESSION[$key] = $value;
+    $_SESSION['_tempdatalist'][$key] = time()+$second;
+}
+private function flashdataclear() {
+    $flashdatatemizlendi = \umutsurmeli\system\library\session::$flashdatatemizlendi;
+    if($flashdatatemizlendi==0) {
+        if(isset($_SESSION['_flashdatalist'])&&!empty($_SESSION['_flashdatalist'])) {
+            foreach ($_SESSION['_flashdatalist'] as $fdkey=>$val) {
+                if($val==1) {
+                    unset($_SESSION[$fdkey]);
+                    unset($_SESSION['_flashdatalist'][$fdkey]);
+                }
+                else if($val==0) {
+                    $_SESSION['_flashdatalist'][$fdkey] = 1;
+                }
+            }
+            
+        }
+      \umutsurmeli\system\library\session::$flashdatatemizlendi=1;
+    }
+}
+private function tempdataclear() {
+    if(isset($_SESSION['_tempdatalist'])&&!empty($_SESSION['_tempdatalist'])) {
+        foreach($_SESSION['_tempdatalist'] as $key=>$val) {
+            if(time() > $val) {
+                unset($_SESSION[$key]);
+                unset($_SESSION['_tempdatalist'][$key]);
             }
         }
-}
-public function clear()
-{
-
-}
-public function flashdata($key,$value=null) {
-    
+    }
 }
 public function destroy() {
     session_unset();
